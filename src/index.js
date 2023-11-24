aa4ed971f8bb13e4dd9011cd96eed38960e7c530
g-ka <gkaphilathithiya@gmail.com>
2023-11-25 00:54:34 +0530

    first_commit

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { Data_provider } from './context/Data_box';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/tamil_mandram'>
       <Data_provider>
         <Routes>
           <Route path='/*' element={<App />}/>
